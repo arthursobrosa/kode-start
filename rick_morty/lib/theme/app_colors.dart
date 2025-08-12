b2a06b3a1ecc204911c3877d8e7c1fcad8a08bb6
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 09:53:46 -0300

    feat: add app colors

---
+++ rick_morty/lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color appBarColor = const Color(0xff1C1B1F);
+  static Color backgroundColor = Colors.black;
+  static Color leftIconColor = const Color(0xffE6E1E5);
+  static Color rightIconColor = const Color(0xffCAC4D0);
+  static Color cardFooterColor = const Color(0xff87A1FA);
+  static Color textColor = Colors.white;
+}
