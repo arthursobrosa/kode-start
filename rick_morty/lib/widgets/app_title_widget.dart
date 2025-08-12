4722422cacdb79f1d6f3f0e242e01654d4a1da44
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 11:09:43 -0300

    feat: create app title widget

---
+++ rick_morty/lib/widgets/app_title_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rick_morty/theme/text_type.dart';
+
+class AppTitleWidget extends StatelessWidget {
+  const AppTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('RICK AND MORTY API', style: TextType.appTitle.textSyle);
+  }
+}
