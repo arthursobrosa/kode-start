2784a26b7ba2cc56316a301922b70bbe2cf80c39
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 09:58:44 -0300

    feat: add fonts

--- rick_morty/lib/main.dart
+++ rick_morty/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rick_morty/pages/home_page.dart';
 
 void main() {
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme
+        )
+      )
     );
   }
 }
