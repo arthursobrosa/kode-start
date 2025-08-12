4a388e373a096658e2469b02ec66c688bd90cc95
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 10:05:24 -0300

    feat: create home card widget

--- rick_morty/lib/pages/home_page.dart
+++ rick_morty/lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rick_morty/theme/app_colors.dart';
+import 'package:rick_morty/widgets/app_bar_widget.dart';
+import 'package:rick_morty/widgets/home_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBarWidget(isDetailsPage: false),
+      body: ListView.builder(
+        itemBuilder: (context, index) => HomeCardWidget(),
+        itemCount: 3,
+      ),
+    );
   }
 }
