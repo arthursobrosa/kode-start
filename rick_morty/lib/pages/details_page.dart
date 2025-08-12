c5f1cdd256fa574b7d5683eb90284ee4ceda0b1c
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 11:05:01 -0300

    feat: change details page to receive sliver app bar inside custom scroll view

--- rick_morty/lib/pages/details_page.dart
+++ rick_morty/lib/pages/details_page.dart
@@ -1,9 +1,10 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:rick_morty/data/repository.dart';
 import 'package:rick_morty/models/character_model.dart';
 import 'package:rick_morty/models/episode_model.dart';
 import 'package:rick_morty/theme/app_colors.dart';
-import 'package:rick_morty/widgets/app_bar_widget.dart';
+import 'package:rick_morty/widgets/sliver_app_bar_widget.dart';
 import 'package:rick_morty/widgets/detailed_card_widget.dart';
 import 'package:rick_morty/widgets/shimmer_widget.dart';
 
@@ -35,29 +36,35 @@ class DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      appBar: AppBarWidget(
-        isDetailsPage: true,
-        onTapLeftIcon: () {
-          Navigator.pop(context);
-        },
-      ),
-      body: FutureBuilder(
-        future: episodeFuture,
-        builder: (context, AsyncSnapshot<EpisodeModel?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Padding(
-              padding: EdgeInsets.only(top: 17, left: 20, right: 20),
-              child: ShimmerWidget.rectangular(height: 500, borderRadius: 10),
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Erro: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            final episode = snapshot.data! as EpisodeModel?;
-            return DetailedCardWidget(character: widget.character, episode: episode);
-          } else {
-            return Center(child: Text('Nenhum dado encontrado'));
-          }
-        },
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBarWidget(
+            isDetailsPage: true,
+            onTapLeftIcon: () => Navigator.of(context).pop(),
+            onTapRightIcon: () => Void,
+          ),
+
+          SliverToBoxAdapter(
+            child: FutureBuilder(
+              future: episodeFuture, 
+              builder: (context, AsyncSnapshot<EpisodeModel?> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Padding(
+                    padding: EdgeInsets.only(top: 17, left: 20, right: 20),
+                    child: ShimmerWidget.rectangular(height: 500, borderRadius: 10),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Erro: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  final episode = snapshot.data! as EpisodeModel?;
+                  return DetailedCardWidget(character: widget.character, episode: episode);
+                } else {
+                  return Center(child: Text('Nenhum dado encontrado'));
+                }
+              }
+            ),
+          )
+        ],
       ),
     );
   }
