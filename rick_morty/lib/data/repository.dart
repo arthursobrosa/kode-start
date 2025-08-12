ef2197dee77e5dbd32e896eb706f07c5f4b71bb9
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 11:16:15 -0300

    refactor: get paginated characters function changed to receive queryParameters

--- rick_morty/lib/data/repository.dart
+++ rick_morty/lib/data/repository.dart
@@ -7,10 +7,20 @@ abstract class Repository {
     BaseOptions(baseUrl: 'https://rickandmortyapi.com/api'),
   );
 
-  static Future<PaginatedCharacters> getPaginatedCharacters({required int page}) async {
+  static Future<PaginatedCharacters> getPaginatedCharacters({
+    required int page,
+    required (String, dynamic)? property,
+  }) async {
+    Map<String, dynamic> queryParameters = {};
+    queryParameters['page'] = page;
+
+    if (property != null) {
+      queryParameters[property.$1] = property.$2;
+    }
+
     var response = await _dio.get(
       '/character',
-      queryParameters: {'page': page},
+      queryParameters: queryParameters,
     );
 
     return PaginatedCharacters.fromJson(response.data);
@@ -24,5 +34,21 @@ abstract class Repository {
   static Future<EpisodeModel> getEpisode(int episodeId) async {
     var response = await _dio.get('/episode/$episodeId');
     return EpisodeModel.fromJson(response.data);
+  }
+
+  static Future<CharacterModel> getCharacterWithEpisode(
+    CharacterModel character,
+  ) async {
+    int? episodeId = character.firstEpisodeId;
+    String firstSeenIn = 'Unknown';
+
+    if (episodeId != null) {
+      EpisodeModel episode = await getEpisode(episodeId);
+      firstSeenIn = episode.name;
+    }
+
+    character.firstSeenIn = firstSeenIn;
+
+    return character;
   }
 }
