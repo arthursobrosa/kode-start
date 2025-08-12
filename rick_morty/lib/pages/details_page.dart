8ff83523e09c2787c2a87dfe914fb49ae5117991
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 10:25:59 -0300

    feat: pages get items from api

--- rick_morty/lib/pages/details_page.dart
+++ rick_morty/lib/pages/details_page.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:rick_morty/data/repository.dart';
+import 'package:rick_morty/models/character_model.dart';
 import 'package:rick_morty/theme/app_colors.dart';
 import 'package:rick_morty/widgets/app_bar_widget.dart';
 import 'package:rick_morty/widgets/detailed_card_widget.dart';
 
 class DetailsPage extends StatefulWidget {
   static const routeId = '/details';
-  const DetailsPage({super.key});
+  const DetailsPage({super.key, required this.characterId});
+
+  final int characterId;
 
   @override
   State<DetailsPage> createState() => DetailsPageState();
 }
 
 class DetailsPageState extends State<DetailsPage> {
+  Future<CharacterModel>? character;
+
+  @override
+  void initState() {
+    character = Repository.getCharacter(widget.characterId);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +34,20 @@ class DetailsPageState extends State<DetailsPage> {
           Navigator.pop(context);
         },
       ),
-      body: DetailedCardWidget(),
+      body: FutureBuilder(
+        future: character,
+        builder: (context, AsyncSnapshot<CharacterModel> snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data!;
+
+            return DetailedCardWidget(character: data);
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
--- rick_morty/lib/pages/home_page.dart
+++ rick_morty/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:rick_morty/models/character_model.dart';
+import 'package:rick_morty/data/repository.dart';
 import 'package:rick_morty/pages/details_page.dart';
 import 'package:rick_morty/theme/app_colors.dart';
 import 'package:rick_morty/widgets/app_bar_widget.dart';
@@ -15,25 +16,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<PaginatedCharacters>? characters;
+
+  @override
+  void initState() {
+    characters = Repository.getPaginatedCharacters();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      appBar: AppBarWidget(
-        isDetailsPage: false,
-        onTapLeftIcon: () => Void,
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return HomeCardWidget(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                DetailsPage.routeId
-              );
-            },
-          );
+      appBar: AppBarWidget(isDetailsPage: false, onTapLeftIcon: () => Void),
+      body: FutureBuilder(
+        future: characters,
+        builder: (context, AsyncSnapshot<PaginatedCharacters> snapshot) {
+          if (snapshot.hasData) {
+            final dataResults = snapshot.data!.results;
+
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return HomeCardWidget(
+                  character: dataResults[index],
+                  onTap: () {
+                    Navigator.of(context).pushNamed(
+                      DetailsPage.routeId,
+                      arguments: dataResults[index].id,
+                    );
+                  },
+                );
+              },
+              itemCount: dataResults.length,
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
-        itemCount: 3,
       ),
     );
   }
