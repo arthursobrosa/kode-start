1ca2367486a08cba5cff0248ce72d943cb9e45da
Arthur Lopes <arthurpsl99@gmail.com>
2025-08-12 11:28:10 -0300

    feat: create view model for home page

--- rick_morty/lib/pages/home_page.dart
+++ rick_morty/lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:rick_morty/models/character_model.dart';
 import 'package:rick_morty/data/repository.dart';
 import 'package:rick_morty/pages/details_page.dart';
+import 'package:rick_morty/pages/home_view_model.dart';
 import 'package:rick_morty/theme/app_colors.dart';
 import 'package:rick_morty/widgets/empty_widget.dart';
 import 'package:rick_morty/widgets/filters_widget.dart';
@@ -25,148 +26,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<CharacterModel> characters = [];
-  bool isLoading = false;
-  bool isConnecting = true;
-  late int numberOfPages;
-  int page = 1;
+  final HomeViewModel _viewModel = HomeViewModel();
+
   final ScrollController _scrollController = ScrollController();
-  bool _isAppBarCollapsed = false;
-  bool _isTextFieldShowing = false;
-  String text = '';
-  (String, dynamic)? queryParameterTuple;
   final TextEditingController _textEditingController = TextEditingController();
-  List<String> filters = CharacterPropertyType.values
-      .map((element) => element.name)
-      .toList();
-  late String selectedFilter;
-  bool _isShowingError = false;
-  ApiException? error;
-  final FocusNode focusNode = FocusNode();
+  final FocusNode _focusNode = FocusNode();
+
+  late String _selectedFilter;
+  String _textFieldText = '';
+  bool _isTextFieldShowing = false;
+  bool _isAppBarCollapsed = false;
+
   Timer? _debounce;
 
   @override
   void initState() {
-    selectedFilter = filters.first;
-
-    fetch();
-
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        fetch();
-      }
-    });
-
-    _scrollController.addListener(() {
-      double expandedHeight = SliverAppBarWidget.expandedHeight;
-      double toolbarHeight = SliverAppBarWidget.toolbarHeight;
-
-      bool isExpanded =
-          _scrollController.hasClients &&
-          _scrollController.offset < (expandedHeight - toolbarHeight);
-
-      if (isExpanded == _isAppBarCollapsed) {
-        setState(() {
-          _isAppBarCollapsed = !isExpanded;
-
-          if (!_isAppBarCollapsed && _isTextFieldShowing && text.isEmpty) {
-            _isTextFieldShowing = false;
-          }
-        });
-      }
-    });
-
     super.initState();
+
+    _setInitialSelectedFilter();
+    _fetch();
+
+    _scrollController.addListener(_scrollListener);
+    _scrollController.addListener(_appBarListener);
   }
 
-  Future<void> fetch() async {
+  void _setInitialSelectedFilter() {
+    final filters = CharacterPropertyType.values
+        .map((element) => element.name)
+        .toList();
+
+    _selectedFilter = filters.first;
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _fetch();
+    }
+  }
+
+  void _appBarListener() {
+    double expandedHeight = SliverAppBarWidget.expandedHeight;
+    double toolbarHeight = SliverAppBarWidget.toolbarHeight;
+
+    bool isExpanded =
+        _scrollController.hasClients &&
+        _scrollController.offset < (expandedHeight - toolbarHeight);
+
+    if (isExpanded == _isAppBarCollapsed) {
+      setState(() {
+        _isAppBarCollapsed = !isExpanded;
+
+        if (!_isAppBarCollapsed &&
+            _isTextFieldShowing &&
+            _textFieldText.isEmpty) {
+          _isTextFieldShowing = false;
+        }
+      });
+    }
+  }
+
+  Future<void> _fetch() async {
     try {
-      await setNumberOfPages();
-      await fetchCharacters();
+      await _viewModel.fetch();
     } on ApiException catch (error) {
       if (kDebugMode) {
         print(error);
       }
 
+      _viewModel.setError(error);
+
       setState(() {
-        this.error = error;
-        _isShowingError = true;
+        _textFieldText = '';
+        _textEditingController.text = _textFieldText;
         _isTextFieldShowing = false;
-        _isAppBarCollapsed = false;
-        text = '';
-        _textEditingController.text = text;
-        queryParameterTuple = null;
       });
+
+      _focusNode.unfocus();
     }
 
-    setState(() {
-      isConnecting = false;
-    });
-  }
-
-  Future<void> setNumberOfPages() async {
-    try {
-      final paginatedCharacters = await Repository.fetchEntity(
-        CharacterModel.endPoint,
-        (json) => PaginatedCharacters.fromJson(json),
-        page: 1,
-        property: queryParameterTuple,
-      );
-
-      numberOfPages = paginatedCharacters.numberOfPages;
-    } on ApiException {
-      rethrow;
-    }
-  }
-
-  Future<void> fetchCharacters() async {
-    if (page >= numberOfPages) {
-      setState(() {
-        isLoading = false;
-      });
-      return;
-    }
-
-    if (isLoading) return;
-
-    setState(() {
-      isLoading = true;
-    });
-
-    try {
-      final paginatedCharacters = await Repository.fetchEntity(
-        CharacterModel.endPoint,
-        (json) => PaginatedCharacters.fromJson(json),
-        page: page,
-        property: queryParameterTuple,
-      );
-
-      setState(() {
-        characters.addAll(paginatedCharacters.results);
-        page++;
-        isLoading = false;
-      });
-    } on ApiException {
-      rethrow;
-    }
+    _viewModel.changeConnection(isConnecting: false);
   }
 
   Future<void> _refresh() async {
     await WidgetsBinding.instance.endOfFrame;
 
-    setState(() {
-      characters = [];
-      isLoading = false;
-      isConnecting = true;
-      _isShowingError = false;
-      page = 1;
-    });
+    _viewModel.resetHomeData();
 
-    await fetch();
+    await _fetch();
   }
 
-  void collapseAppBar() {
+  void _collapseAppBar() {
     final expandedHeight = SliverAppBarWidget.expandedHeight;
     final toolbarHeight = SliverAppBarWidget.toolbarHeight;
 
@@ -183,214 +133,212 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void onChanged(String text) {
-    setState(() {
-      this.text = text;
-    });
-
-    _debounce?.cancel();
-
-    _debounce = Timer(Duration(milliseconds: 500), () {
-      queryParameterTuple = (selectedFilter, text);
-      _refresh();
-    });
-  }
-
-  void onEditingComplete() {
-    focusNode.unfocus();
-    queryParameterTuple = (selectedFilter, text);
-    _refresh();
-  }
-
-  void onFilterSelected(String filter) {
-    setState(() {
-      selectedFilter = filter;
-    });
-
-    onEditingComplete();
-  }
-
-  void openFilters() {
+  void _openFilters() {
     showModalBottomSheet(
       context: context,
       backgroundColor: AppColors.appBarColor,
       isScrollControlled: true,
-      builder: (BuildContext context) {
+      builder: (context) {
         return SizedBox(
           width: MediaQuery.of(context).size.width,
           child: FiltersWidget(
             filters: CharacterPropertyType.values
                 .map((element) => element.name)
                 .toList(),
-            initialSelectedFilter: selectedFilter,
-            onFilterSelected: onFilterSelected,
+            initialSelectedFilter: _selectedFilter,
+            onFilterSelected: _onFilterSelected,
           ),
         );
       },
     );
   }
 
-  void refreshAndCollapse() {
+  void _onChanged(String text) {
+    setState(() {
+      _textFieldText = text;
+    });
+
+    _debounce?.cancel();
+
+    _debounce = Timer(Duration(milliseconds: 500), () {
+      _viewModel.updateQuery(
+        selectedFilter: _selectedFilter,
+        textFieldText: _textFieldText,
+      );
+
+      _refresh();
+    });
+  }
+
+  void _onEditingComplete() {
+    _focusNode.unfocus();
+
+    _viewModel.updateQuery(
+      selectedFilter: _selectedFilter,
+      textFieldText: _textFieldText,
+    );
+
     _refresh();
-    collapseAppBar();
   }
 
-  String? getEmptyText() {
-    if (error?.statusCode == 404) {
-      return 'No character found with that $selectedFilter.';
-    }
+  void _onFilterSelected(String filter) {
+    setState(() {
+      _selectedFilter = filter;
+    });
 
-    return error?.message;
-  }
-
-  void tryAgain() {
-    fetch();
+    _onEditingComplete();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      body: RefreshIndicator(
-        color: Colors.white,
-        backgroundColor: AppColors.cardFooterColor,
-        onRefresh: _refresh,
-        child: CustomScrollView(
-          controller: _scrollController,
-          slivers: [
-            SliverAppBarWidget(
-              leftIcon: _isAppBarCollapsed && _isTextFieldShowing
-                  ? SizedBox.shrink()
-                  : IconButton(
-                      onPressed: () => Void,
-                      icon: Icon(
-                        Icons.menu,
-                        color: AppColors.leftIconColor,
-                        size: 24,
-                      ),
-                    ),
-              actions: _isAppBarCollapsed
-                  ? [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 6),
-                        child: IconButton(
-                          onPressed: _isTextFieldShowing
-                              ? openFilters
-                              : collapseAppBar,
-                          icon: Icon(
-                            _isTextFieldShowing
-                                ? Icons.filter_list
-                                : Icons.search,
-                            color: _isTextFieldShowing
-                                ? AppColors.cardFooterColor
-                                : AppColors.rightIconColor,
-                            size: 24,
-                          ),
-                        ),
-                      ),
-                    ]
-                  : [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: IconButton(
-                          onPressed: _isShowingError
-                              ? refreshAndCollapse
-                              : collapseAppBar,
-                          icon: Icon(
-                            Icons.search,
-                            color: _isTextFieldShowing
-                                ? AppColors.cardFooterColor
-                                : AppColors.rightIconColor,
-                            size: 24,
-                          ),
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(right: 6),
-                        child: IconButton(
+    return ValueListenableBuilder<HomeData>(
+      valueListenable: _viewModel.homeData,
+      builder: (_, data, _) {
+        return Scaffold(
+          backgroundColor: AppColors.backgroundColor,
+          body: RefreshIndicator(
+            color: Colors.white,
+            backgroundColor: AppColors.cardFooterColor,
+            onRefresh: _refresh,
+            child: CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                SliverAppBarWidget(
+                  leftIcon: _isAppBarCollapsed && _isTextFieldShowing
+                      ? SizedBox.shrink()
+                      : IconButton(
                           onPressed: () => Void,
                           icon: Icon(
-                            CupertinoIcons.person_crop_circle,
-                            color: AppColors.rightIconColor,
+                            Icons.menu,
+                            color: AppColors.leftIconColor,
                             size: 24,
                           ),
                         ),
-                      ),
-                    ],
-              titleWidget: _isAppBarCollapsed && _isTextFieldShowing
-                  ? TextFieldWidget(
-                      onChanged: onChanged,
-                      onEditingComplete: onEditingComplete,
-                      controller: _textEditingController,
-                      focusNode: focusNode,
-                    )
-                  : AppTitleWidget(),
-            ),
-
-            if (isConnecting)
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(
-                      right: 20,
-                      left: 20,
-                      top: 15,
-                    ),
-                    child: ShimmerWidget.rectangular(
-                      height: 160,
-                      borderRadius: 10,
-                    ),
-                  );
-                }),
-              )
-            else if (_isShowingError)
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: EmptyWidget(text: getEmptyText(), onTapButton: _refresh),
-              )
-            else
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  if (index == characters.length) {
-                    if (isLoading) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 15),
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            color: AppColors.cardFooterColor,
+                  actions: _isAppBarCollapsed
+                      ? [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 6),
+                            child: IconButton(
+                              onPressed: _isTextFieldShowing
+                                  ? _openFilters
+                                  : _collapseAppBar,
+                              icon: Icon(
+                                _isTextFieldShowing
+                                    ? Icons.filter_list
+                                    : Icons.search,
+                                color: _isTextFieldShowing
+                                    ? AppColors.cardFooterColor
+                                    : AppColors.rightIconColor,
+                                size: 24,
+                              ),
+                            ),
                           ),
+                        ]
+                      : [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: IconButton(
+                              onPressed: () {
+                                if (data.isShowingError) {
+                                  _refresh();
+                                }
+
+                                _collapseAppBar();
+                              },
+                              icon: Icon(
+                                Icons.search,
+                                color: _isTextFieldShowing
+                                    ? AppColors.cardFooterColor
+                                    : AppColors.rightIconColor,
+                                size: 24,
+                              ),
+                            ),
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.only(right: 6),
+                            child: IconButton(
+                              onPressed: () => Void,
+                              icon: Icon(
+                                CupertinoIcons.person_crop_circle,
+                                color: AppColors.rightIconColor,
+                                size: 24,
+                              ),
+                            ),
+                          ),
+                        ],
+                  titleWidget: _isAppBarCollapsed && _isTextFieldShowing
+                      ? TextFieldWidget(
+                          onChanged: _onChanged,
+                          onEditingComplete: _onEditingComplete,
+                          controller: _textEditingController,
+                          focusNode: _focusNode,
+                        )
+                      : AppTitleWidget(),
+                ),
+
+                if (data.isConnecting)
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          right: 20,
+                          left: 20,
+                          top: 15,
+                        ),
+                        child: ShimmerWidget.rectangular(
+                          height: 160,
+                          borderRadius: 10,
                         ),
                       );
-                    }
+                    }),
+                  )
+                else if (data.isShowingError)
+                  SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: EmptyWidget(
+                      text: _viewModel.getEmptyText(
+                        selectedFilter: _selectedFilter,
+                      ),
+                      onTapButton: _refresh,
+                    ),
+                  )
+                else
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      if (index == data.characters.length) {
+                        if (data.isLoading) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: AppColors.cardFooterColor,
+                              ),
+                            ),
+                          );
+                        }
 
-                    return SizedBox(height: 40);
-                  }
+                        return SizedBox(height: 40);
+                      }
 
-                  final character = characters[index];
+                      final character = data.characters[index];
 
-                  return HomeCardWidget(
-                    character: character,
-                    onTap: () {
-                      Navigator.of(
-                        context,
-                      ).pushNamed(DetailsPage.routeId, arguments: character);
-                    },
-                  );
-                }, childCount: characters.length + 1),
-              ),
-          ],
-        ),
-      ),
+                      return HomeCardWidget(
+                        character: character,
+                        onTap: () {
+                          Navigator.of(context).pushNamed(
+                            DetailsPage.routeId,
+                            arguments: character,
+                          );
+                        },
+                      );
+                    }, childCount: data.characters.length + 1),
+                  ),
+              ],
+            ),
+          ),
+        );
+      },
     );
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    _textEditingController.dispose();
-    focusNode.dispose();
-    _debounce?.cancel();
-    super.dispose();
   }
 }
---
+++ rick_morty/lib/pages/home_view_model.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/foundation.dart';
+import 'package:rick_morty/data/repository.dart';
+import 'package:rick_morty/models/character_model.dart';
+
+class HomeData {
+  List<CharacterModel> characters;
+  bool isLoading;
+  bool isConnecting;
+  bool isShowingError;
+
+  HomeData({
+    required this.characters,
+    required this.isLoading,
+    required this.isConnecting,
+    required this.isShowingError,
+  });
+
+  HomeData copyWith({
+    List<CharacterModel>? characters,
+    bool? isLoading,
+    bool? isConnecting,
+    bool? isShowingError,
+  }) {
+    return HomeData(
+      characters: characters ?? this.characters,
+      isLoading: isLoading ?? this.isLoading,
+      isConnecting: isConnecting ?? this.isConnecting,
+      isShowingError: isShowingError ?? this.isShowingError,
+    );
+  }
+}
+
+class HomeViewModel {
+  ValueNotifier<HomeData> homeData = ValueNotifier(
+    HomeData(
+      characters: [],
+      isLoading: false,
+      isConnecting: true,
+      isShowingError: false,
+    ),
+  );
+
+  (String, dynamic)? _queryParameterTuple;
+  int _numberOfPages = 0;
+  int _page = 1;
+  ApiException? _error;
+
+  Future<void> fetch() async {
+    try {
+      await setNumberOfPages();
+      await fetchCharacters();
+    } on ApiException {
+      rethrow;
+    }
+  }
+
+  Future<void> setNumberOfPages() async {
+    try {
+      final paginatedCharacters = await Repository.fetchEntity(
+        CharacterModel.endPoint,
+        (json) => PaginatedCharacters.fromJson(json),
+        page: 1,
+        property: _queryParameterTuple,
+      );
+
+      _numberOfPages = paginatedCharacters.numberOfPages;
+    } on ApiException {
+      rethrow;
+    }
+  }
+
+  Future<void> fetchCharacters() async {
+    if (_page > _numberOfPages) {
+      homeData.value = homeData.value.copyWith(isLoading: false);
+      return;
+    }
+
+    if (homeData.value.isLoading) return;
+
+    homeData.value = homeData.value.copyWith(isLoading: true);
+
+    try {
+      final paginatedCharacters = await Repository.fetchEntity(
+        CharacterModel.endPoint,
+        (json) => PaginatedCharacters.fromJson(json),
+        page: _page,
+        property: _queryParameterTuple,
+      );
+
+      final updatedCharacters = List<CharacterModel>.from(
+        homeData.value.characters,
+      )..addAll(paginatedCharacters.results);
+
+      _page++;
+
+      homeData.value = homeData.value.copyWith(
+        characters: updatedCharacters,
+        isLoading: false,
+      );
+    } on ApiException {
+      rethrow;
+    }
+  }
+
+  void resetHomeData() {
+    homeData.value = homeData.value.copyWith(
+      characters: [],
+      isLoading: false,
+      isConnecting: true,
+      isShowingError: false,
+    );
+
+    _page = 1;
+  }
+
+  void updateQuery({
+    required String selectedFilter,
+    required String textFieldText,
+  }) {
+    _queryParameterTuple = (selectedFilter, textFieldText);
+  }
+
+  String? getEmptyText({required String selectedFilter}) {
+    if (_error?.statusCode == 404) {
+      return 'No character found with that $selectedFilter';
+    }
+
+    return _error?.message;
+  }
+
+  void setError(ApiException error) {
+    _error = error;
+    homeData.value = homeData.value.copyWith(isShowingError: true);
+    _queryParameterTuple = null;
+  }
+
+  void changeConnection({required bool isConnecting}) {
+    homeData.value = homeData.value.copyWith(isConnecting: isConnecting);
+  }
+}
